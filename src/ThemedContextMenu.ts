edfb38db7046e883b4756231620e11cc5e873405
Andres-XL <amovilla1103@gmail.com>
2021-03-01 13:59:04 -0500

    remove unnecessary on blur listener

--- src/ThemedContextMenu.ts
+++ src/ThemedContextMenu.ts
@@ -6,24 +6,12 @@ export class ThemedContextMenu {
     private activeContextMenu: HTMLDivElement
     private visible: boolean = false
     private children: MenuItem[] = []
-    private windowBlurObserver: MutationObserver
 
     constructor() {
         // add click listener to clear the context menu
         document.addEventListener("click", (e) =>
             this.onMouseClick(e as MouseEvent),
         )
-
-        // add blur listener to clear the context menu
-        const body = document.querySelector("body")
-        this.windowBlurObserver = new MutationObserver((e) =>
-            this.windowBlurCallback(e, this),
-        )
-        if (body) {
-            this.windowBlurObserver.observe(body, {
-                attributeFilter: ["class"],
-            })
-        }
 
         // create the context menu, but make it invisible
         this.activeContextMenu = document.createElement("div")
@@ -68,12 +56,6 @@ export class ThemedContextMenu {
         )
     }
 
-    private windowBlurCallback(mutation, tcm: ThemedContextMenu) {
-        // need to pass in the tcm through params because 'this' is the
-        // MutationObserver in this function
-        // tcm.deleteContextMenu()
-    }
-
     // adds a context menu item to context menu
     private addChild(item) {
         const mitem = MenuItem.createMenuItem(item, this)
@@ -111,7 +93,6 @@ export class ThemedContextMenu {
             this.visible = false
             this.children = []
             this.activeContextMenu.classList.add("invisible")
-            this.lastClick = undefined
             this.removeAllChildNodes()
         }
     }
